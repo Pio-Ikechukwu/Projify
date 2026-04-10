58305bb92ff18e3781634ab790d59fdc1c919af9
pioikechukwu1@gmail.com <pioikechukwu@gmail.com>
2026-04-10 13:56:06 +0100

    chore: use node 24, add DB_PORT to env

--- mytodobackend/config/config.js
+++ mytodobackend/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -6,20 +6,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
 };
